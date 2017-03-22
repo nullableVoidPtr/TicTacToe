7fc233c8e109d0d3b72302d2659c61ba6b214c5a
N.R.T <nathsharwor@hotmail.com>
2017-03-22 17:52:36 +0800

    v1.0.1
    Cleanup and reorganise.

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
 /*
- ______
-|      |
-|  \/  |
-|  /\  |
-|______|
+ * MAIN.JS
+ * 
+ * Javascript functions and executable code for 'Ultimate Tic Tac Toe'.
+ * See www.github.com/met4000/TicTacToe for development of this.
+ * 
+ */
 
- ______
-|  __  |
-| |  | |
-| |__| |
-|______|
-*/
-
-// Contains a table of the X and O ascii values
-valueArr = {};
-valueArr.x = ["______", "      ", "  \\\/  ", "  \/\\  ", "______"];
-valueArr.o = ["______", "  __  ", " |  | ", " |__| ", "______"];
-
-active = {};
-active.symbol = 'x';
-active.toggle = function () {
+/*
+ * Contains all data for the game
+ */
+game = {};
+game.version = "1.0.1";
+game.board = {};
+for (var z = 0; z < 1; z++) {
+	game.board[z] = {};
+	for (var a = 0; a < 1; a++) {
+		game.board[z][a] = {};
+		game.board[z][a].data = {};
+		game.board[z][a].data.locations = {};
+		for (var x = 0; x < 3; x++) {
+			game.board[z][a].data.locations[x] = {};
+			for (var y = 0; y < 3; y++) {
+				game.board[z][a].data.locations[x][y] = "";
+			}
+		}
+		game.board[z][a].data.winner = "";
+	}
+}
+game.active = {};
+game.active.symbol = 'x';
+game.active.toggle = function () {
 	if (this.symbol.toLowerCase() == 'x')
 		this.symbol = 'o';
 	else if (this.symbol.toLowerCase() == 'o')
 		this.symbol = 'x';
 };
+
+// Contains a table of the X and O ascii values
+valueArr = {};
+valueArr.x = ["______", "      ", "  \\\/  ", "  \/\\  ", "______"];
+valueArr.o = ["______", "  __  ", " |  | ", " |__| ", "______"];
 
 onload = function () {
 	//Document Load Trigger
@@ -33,7 +48,7 @@ onload = function () {
 			for (var y = 0; y < 3; y++) {
 				for (var x = 0; x < 3; x++) {
 					for (var i = 1, buttons = document.getElementsByClassName("boardButton" + a + "_" + z + "_" + y + "_" + x); i < 4; i++) {
-						buttons[i].addEventListener("click", function(){updateInner(this.className.charAt(17), this.className.charAt(15), this.className.charAt(13), this.className.charAt(11), active.symbol); active.toggle(); updateWinner(this.className.charAt(13), this.className.charAt(11));});
+						buttons[i].addEventListener("click", function(){updateInner(this.className.charAt(17), this.className.charAt(15), this.className.charAt(13), this.className.charAt(11), game.active.symbol); game.active.toggle(); updateWinner(this.className.charAt(13), this.className.charAt(11));});
 					}
 				}
 			}
@@ -52,7 +67,7 @@ onload = function () {
 function updateInner(x, y, z, a, value) {
 	if (!(value.toLowerCase() == 'x' || value.toLowerCase() == 'o'))
 		return false;
-	for (i = 1, buttons = document.getElementsByClassName("boardButton" + a + "_" + z + "_" + y + "_" + x); i < 4; i++) {
+	for (var i = 1, buttons = document.getElementsByClassName("boardButton" + a + "_" + z + "_" + y + "_" + x); i < 4; i++) {
 		buttons[i].innerHTML = valueArr[value.toLowerCase()][i];
 		game.board[z][a].data.locations[x][y] = value.toLowerCase();
 	}
@@ -71,7 +86,7 @@ function same(a, b) {
 	if (arguments.length < 2)
 		return arguments.length == 1;
 	for (var i = 1; i < arguments.length; i++)
-		if (arguments[0].toLowerCase() != arguments[i].toLowerCase() || arguments[i] == '')
+		if (arguments[0].toLowerCase() != arguments[i].toLowerCase() || !arguments[i])
 			return false;
 	return true;
 }
@@ -82,6 +97,8 @@ function same(a, b) {
  * a - The a location of the board
  */
 function updateWinner(z, a) {
+	if (!!game.board[z][a].data.winner)
+		return false;
 	var board = game.board[z][a].data.locations, win = '';
 	
 	//Vertical
@@ -107,25 +124,4 @@ function updateWinner(z, a) {
 		win = board[2][0].toLowerCase();
 	
 	game.board[z][a].data.winner = win;
-}
-
-/*
- * Contains all data for the game
- */
-game = {};
-game.board = {};
-for (var z = 0; z < 1; z++) {
-	game.board[z] = {};
-	for (var a = 0; a < 1; a++) {
-		game.board[z][a] = {};
-		game.board[z][a].data = {};
-		game.board[z][a].data.locations = {};
-		for (var x = 0; x < 3; x++) {
-			game.board[z][a].data.locations[x] = {};
-			for (var y = 0; y < 3; y++) {
-				game.board[z][a].data.locations[x][y] = "";
-			}
-		}
-		game.board[z][a].data.winner = "";
-	}
 }
